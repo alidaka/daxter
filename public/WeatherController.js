4f5bf1c5ec0217e5aa0d591ed76e26103a4134a9
alidaka <alidaka@gmail.com>
2017-09-24 18:29:29 -0700

    add color to weather chart

--- public/WeatherController.js
+++ public/WeatherController.js
@@ -12,32 +12,40 @@
       dom.className = "item";
       dom.style["flex-direction"] = "column";
 
-      this._get("/weather", this._updateWeather, dom);
+      this._get("/weather", this._updateWeather.bind(this), dom);
       return dom;
     };
 
     _updateWeather(weather, dom) {
       var title = document.createElement("div");
-      title.innerHTML = weather.todayHigh.toFixed(0) + "° high<br/>" + weather.summary + " today";
+      title.innerHTML = Math.trunc(weather.current) + "°<br/>" + weather.summary + " today";
       title.className = "item";
       title.style["flex"] = 1;
       dom.appendChild(title);
 
       var filler = document.createElement("div");
-      title.className = "item";
       filler.style["flex"] = 2;
       dom.appendChild(filler);
 
+      this._attachChart(weather, dom);
+    };
+
+    _attachChart(weather, dom) {
       var data = weather.hourly.map(function(hour) {
         return { t: hour.time * 1000, y: hour.temperature };
       }).slice(0, 16);
+
+      var high = weather.todayHigh;
+      var blue = 255 * (1 - (Math.max(0, high - 40) / 40));
+      var red = 255 * (Math.max(0, high - 40) / 40);
+      var backgroundColor = "rgba(" + Math.trunc(red) + ", 0, " + Math.trunc(blue) + ", .5)";
 
       var config = {
         type: "line",
         data: {
           datasets: [{
             label: false,
-            backgroundColor: "rgba(255, 255, 255, .25)",
+            backgroundColor: backgroundColor,
             data: data,
             fill: "origin"
           }]
