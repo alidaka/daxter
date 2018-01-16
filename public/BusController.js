cf91ed2f88ac31c1fdad46733ccb3d1384cf47ee
alidaka <alidaka@gmail.com>
2018-01-16 11:09:39 -0500

    WIP: begin layout for buses

--- public/BusController.js
+++ public/BusController.js
@@ -9,9 +9,20 @@
     createDom() {
       var dom = document.createElement("div");
       dom.className = "item";
+      dom.classList.add("buses");
 
-      dom.innerHTML = "bus";
+      this._get("/buses", this._updateBuses.bind(this), dom);
       return dom;
+    };
+
+    _updateBuses(buses, dom) {
+      for (var i = 0; i < buses.buses.length; i++) {
+        var bus = buses.buses[i];
+        var busDom = document.createElement("div");
+        busDom.className = "container";
+        busDom.innerHTML = `${bus.status} ${bus.time} ${bus.delta}`;
+        dom.appendChild(busDom);
+      };
     };
   };
 })();
--- src/app.js
+++ src/app.js
@@ -24,7 +24,7 @@ app.get('/weather', function (req, res) {
       };
     };
 
-    var conciseResponse = {
+    var response = {
       'currentTemp': json.currently.temperature,
       'currentSummary': json.currently.summary,
       'high': json.daily.data[0].temperatureMax,
@@ -33,18 +33,47 @@ app.get('/weather', function (req, res) {
       'hourly': json.hourly.data.map(hourify)
     };
 
-    res.json(conciseResponse);
+    res.json(response);
   });
 });
 
 app.get('/buses', function (req, res) {
   var endpoint = process.env.OBA_ENDPOINT;
   var key = process.env.OBA_KEY;
+  // TODO: other stops? time estimates for walking to them?
   var stopId = '1_259'; // TODO: verify stop ID?
 
   var uri = `${endpoint}/where/arrivals-and-departures-for-stop/${stopId}.json?key=${key}`;
   httpsGet(uri, function(json) {
-    res.json(json);
+    var busify = function(entry) {
+      var status = 'unknown';
+      var time = entry.scheduledArrivalTime;
+      var delta = 0;
+
+      if (entry.predicted && entry.predictedTime !== null) {
+        time = entry.predictedArrivalTime;
+        delta = time - entry.scheduledArrivalTime;
+
+        if (delta > 0)      { status = 'late'; }
+        else if (delta < 0) { status = 'early'; }
+        else                { status = 'scheduled'; }
+      }
+
+      return {
+        'status': status,
+        'time': time,
+        'delta': Math.abs(delta)
+      };
+    };
+
+    // TODO: sort
+    var response = {
+      'buses': json.data.entry.arrivalsAndDepartures
+        .map(busify)
+        .sort((a, b) => a.predictedTime > b.predictedTime)
+    };
+
+    res.json(response);
   });
 });
 
