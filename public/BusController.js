7a18bc6b794cdf64f8e58a58a66262879484d3a1
alidaka <alidaka@gmail.com>
2018-10-21 22:29:24 -0700

    distribute space across bus rows

--- public/BusController.js
+++ public/BusController.js
@@ -18,15 +18,33 @@
 
     _updateBuses(buses, dom) {
       for (var i = 0; i < buses.buses.length; i++) {
-        var bus = buses.buses[i];
-        var time = dateFormat(bus.time, "HH:MM:ss");
-        var delta = dateFormat(new Date(bus.time) - this._date, "M");
-
-        var busDom = document.createElement("div");
-        busDom.className = "item";
-        busDom.innerHTML = `${bus.name} | ${bus.status} | ${delta}m`;
-        dom.appendChild(busDom);
+        dom.appendChild(this._bus(buses.buses[i]));
       };
+    };
+
+    _bus(bus) {
+      var time = dateFormat(bus.time, "HH:MM:ss");
+      var delta = dateFormat(new Date(bus.time) - this._date, "M");
+
+      var dom = document.createElement("div");
+      dom.className = "bus";
+
+      var name = document.createElement("div");
+      name.className = 'item';
+      name.innerHTML = bus.name;
+      dom.appendChild(name);
+
+      var status = document.createElement("div");
+      status.className = 'item';
+      status.innerHTML = bus.status;
+      dom.appendChild(status);
+
+      var deltaDom = document.createElement("div");
+      deltaDom.className = 'item';
+      deltaDom.innerHTML = delta;
+      dom.appendChild(deltaDom);
+
+      return dom;
     };
   };
 })();
